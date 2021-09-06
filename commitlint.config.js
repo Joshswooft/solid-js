ab814730dc68802aeac505cedfa532ae156ae503
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2021-09-06 14:24:54 +0100

    chore: disable max body-line rule for commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
-module.exports = { extends: ["@commitlint/config-conventional",], rules: {
-    "header-max-length": [0] //disable rule
-},
- };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [0], //disable rule
+    "body-max-line-length": [0]
+  },
+};
