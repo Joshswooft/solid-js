2681d87622c10393f6d6b057104d3281a65e65b2
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2021-09-06 14:19:55 +0100

    chore: disable header violation rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = { extends: ["@commitlint/config-conventional",], rules: {
+    "header-max-length": [0] //disable rule
+},
+ };
